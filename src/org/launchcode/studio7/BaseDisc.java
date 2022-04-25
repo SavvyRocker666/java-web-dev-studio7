215f502d56c5a07308f20e298ada81825e94a0a3
Savvy <sabrinarocker16@gmail.com>
2022-04-25 18:50:52 -0500

    added code optical

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,12 @@
+package org.launchcode.studio7;
+
+public class BaseDisc {
+    private String discName;
+    private int discCapacity;
+    private int yearReleased;
+
+    public BaseDisc(String discName, int discCapacity, int yearReleased){
+        this.discName = discName;
+
+    }
+}
--- src/org/launchcode/studio7/OpticalDisc.java
+++ src/org/launchcode/studio7/OpticalDisc.java
@@ -1,7 +1,11 @@
 package org.launchcode.studio7;
 
 public interface OpticalDisc {
+
 int discSpinMaxSpeed = 500;
-    void spinDisc();
-    default int
+
+void spinDisc();
+    default int returnMaxSpeed(){
+        return discSpinMaxSpeed;
+    }
 }
